e04f0537fa2274c62cb4940e6eeab2e66b8717fa
Samuel Adone <syadone@gmail.com>
2021-03-18 16:02:27 +0100

    first commit

---
+++ VCubWatcher/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace VCubWatcher.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
